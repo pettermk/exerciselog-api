bdd8f669e2224237eae6afeffc8b0c2066b8f4dc
Petter Kvalvaag <petter.kvalvaag@proton.me>
2024-05-19 22:07:12 +0200

    Factor out persistence into own file

--- Models/Exercise.cs
+++ Models/Exercise.cs
@@ -6,38 +6,6 @@ using System.Diagnostics.Eventing.Reader;
 
 namespace ExerciseLogApi.Models;
 
-public class Persistence : DbContext
-{
-    public Persistence(DbContextOptions<Persistence> options) : base(options)
-    {
-    }
-    public DbSet<Exercise> Exercises { get; set; }
-    public DbSet<Session> Sessions { get; set; }
-    public DbSet<Set> Sets { get; set; }
-
-    private string ConnectionStringFromEnv() {
-        var configBuilder = new ConfigurationBuilder()
-            .AddEnvironmentVariables()
-            .Build();
-        string? host = configBuilder["PG_HOST"];
-        string? database = configBuilder["PG_DATABASE"];
-        string? user = configBuilder["PG_USER"];
-        string? password = configBuilder["PG_PASSWORD"];
-
-        if (string.IsNullOrEmpty(host) || string.IsNullOrEmpty(database) ||
-               string.IsNullOrEmpty(user) || string.IsNullOrEmpty(password))
-        {
-            return @"Host=localhost;Username=postgres;Password=12345;Database=exerciselog";
-        }
-
-        return $"Host={host};Username={user};Password={password};Database={database}";
-    }
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        string connFromEnv = ConnectionStringFromEnv();
-        optionsBuilder.UseNpgsql(connFromEnv);
-    }
-}
 
 
 public class Exercise
