f7a502f7077805837e44076781d888158d7b0e64
Petter Kvalvaag <petter.kvalvaag@proton.me>
2024-06-30 23:30:21 +0200

    Simplify score model to generic timeseries

--- Models/Timeseries.cs
+++ Models/Timeseries.cs
@@ -3,31 +3,27 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.Extensions.Configuration;
 using System.Diagnostics.Eventing.Reader;
+using System.Text.Json;
 
 using ExerciseLogApi.Models;
 
 namespace ExerciseLogApi.Models;
 
-public class ScoreType
+public class Timeseries: IDisposable
 {
     [Key]
     public int Id { get; set; }
 
     [Required]
-    public required string Name { get; set; }
-
-    public required string Description { get; set; }
-
-}
-
-public class Score
-{
-    [ForeignKey("ScoreType")]
-    public int  ScoreTypeId { get; set; }
-
-    [Required]
     public required DateTime Timestamp { get; set; }
-    [Range(1, 10)]
-    public int Value { get; set; }
-
+    [Required]
+    public required string Dimension { get; set; }
+    [Required]
+    public required double Value { get; set; }
+    [Required]
+    public required List<String> Tags { get; set; }
+    public JsonDocument? Metadata { get; set; }
+    
+    public void Dispose() => Metadata?.Dispose();
 }
+
